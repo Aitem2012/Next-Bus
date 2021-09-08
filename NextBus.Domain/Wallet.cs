cc0c275244565df3bb8a654ec8854192aa781d7e
Aitem2012 <ibrahim.ajumoko@decagon.dev>
2021-09-08 14:26:11 +0100

    base project entity set up

---
+++ NextBus.Domain/Wallet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NextBus.Domain
+{
+    public class Wallet : BaseEntity
+    {
+        public Wallet()
+        {
+            Histories = new HashSet<WalletHistory>();
+        }
+
+        public string AppUserId { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string Reference { get; set; }
+        
+
+        // One to Zero or One Wallet Relationship
+        public virtual AppUser AppUser { get; set; }
+
+        // One Wallet to Many Relationships
+        public virtual ICollection<WalletHistory> Histories { get; set; }
+    }
+}
---
+++ NextBus.Domain/WalletHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NextBus.Domain
+{
+    public class WalletHistory : BaseEntity
+    {
+        public decimal Amount { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Reference { get; set; }
+        public string Details { get; set; }
+        public string WalletAppUserId { get; set; }
+        
+        // One to Many WalletHistories Relationships
+        public virtual Wallet Wallet { get; set; }
+    }
+}
