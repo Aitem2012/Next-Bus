12edff7ff39f586dbc5c12b43a65c05a41675bde
Ibrahim Ajumoko <iajumoko@axxess.com>
2021-10-28 21:50:52 +0100

    Update on Controllers

---
+++ NextBus.Presentation/Buses/Handlers/Queries/GetBusForUserQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NextBus.Application.Interfaces.Persistence;
+using NextBus.Presentation.Buses.Models.Result;
+using NextBus.Presentation.Buses.Queries;
+
+namespace NextBus.Presentation.Buses.Handlers.Queries
+{
+    public class GetBusForUserQueryHandler : IRequestHandler<GetBusForUserQuery, IEnumerable<GetBusQueryResult>>
+    {
+        private readonly IAppDbContext _context;
+        private readonly IMapper _mapper;
+        public GetBusForUserQueryHandler(IAppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<GetBusQueryResult>> Handle(GetBusForUserQuery request, CancellationToken cancellationToken)
+        {
+            var buses = await _context.Buses.Where(x => x.Destination.ToLower() == request.QueryParams.ToLower() || x.TakeOffPoint.ToLower() == request.QueryParams.ToLower() && x.TotalAvailableSeat >0)
+                .ToListAsync(cancellationToken);
+            return _mapper.Map<IEnumerable<GetBusQueryResult>>(buses);
+        }
+    }
+}
---
+++ NextBus.Presentation/Buses/Queries/GetBusForUserQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using NextBus.Presentation.Buses.Models.Result;
+
+namespace NextBus.Presentation.Buses.Queries
+{
+    public class GetBusForUserQuery : IRequest<IEnumerable<GetBusQueryResult>>
+    {
+        public string QueryParams { get; set; }
+    }
+}
