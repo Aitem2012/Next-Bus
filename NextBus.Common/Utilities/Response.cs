deec5b6be04d3418941eeb32a65a3c4173c843a5
Ibrahim Ajumoko <iajumoko@axxess.com>
2021-10-29 07:36:18 +0100

    Security added

---
+++ NextBus.Common/Utilities/Response.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace NextBus.Common.Utilities
+{
+    public class Response<T>
+    {
+        public string Message { get; set; }
+        public Dictionary<string, string> Errs { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ NextBus.Data/Data/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NextBus.Data.Data
+{
+    public class LoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public bool RememberMe { get; set; }
+    }
+}
